5b354ec95075641746a10e480ae5a7a860678344
Equ17y <antonsobol28@gmail.com>
2026-07-02 21:41:16 +0300

    echobot for VK

--- vk_bot.py
+++ vk_bot.py
@@ -8,12 +8,20 @@ VK_TOKEN = os.getenv("VK_GROUP_TOKEN")
 
 vk_session = vk_api.VkApi(token=VK_TOKEN)
 longpoll = VkLongPoll(vk_session)
+vk = vk_session.get_api()
+
+print("VK Эхо-бот запущен и слушает сообщения...")
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
         if event.to_me:
+            print('Новое сообщение:')
             print('Для меня от: ', event.user_id)
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+            print('Текст:', event.text)
+            
+            vk.messages.send(
+                user_id=event.user_id,
+                message=event.text,
+                random_id=0
+            )
+            print('Ответ отправлен\n')
